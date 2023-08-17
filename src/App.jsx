11805e515be59dbaa7855805987ecc7b08a5dd1c
david <palacingimenezdavid.fpllefia@gmail.com>
2023-08-17 16:46:23 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import './App.css'
+
+const TURNS = {
+  X: 'X',
+  O: 'O'
+}
+
+const Square = ({ children, updateBoard, index, isSelected }) => {
+  const className = `cell ${ isSelected ? 'is-selected' : '' }`;
+  const handleClick = () => {
+    updateBoard()
+  }
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = () => {
+    turn === TURNS.X
+    ? setTurn(TURNS.O)
+    : setTurn(TURNS.X)
+  }
+
+  return (
+    <main>
+      <h3>Tic Tac Toe</h3>
+      <section className="board">
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+
+    </main>
+  )
+}
+
+export default App
